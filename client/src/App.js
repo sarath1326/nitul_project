efc83a8ee75568cacd78d89dd5d157dcfc53a8cf
sarath <sarathsarath93366@gmail.com>
2023-10-31 20:23:41 +0530

    err find 9

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Users from './Users'
